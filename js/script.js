70d867c1fd24d92950c38b9d30fb3ce93eafcfad
RT / Fernando Rossetti (TIC) <fernando.rossetti@fm.gob.ar>
2022-10-11 12:10:49 -0300

    Nuevos Cambios

--- js/script.js
+++ js/script.js
@@ -6,21 +6,22 @@ class PlazoFijo {
         this.id = id;
     }
 
-    asignarId(array) {
+    idPlazo(array) {
         this.id = array.length;
     }
 }
 class Prestamo {
-    constructor(monto, id) {
+    constructor(monto, meses, id) {
         this.monto = parseInt(monto);
+        this.meses = parseInt(meses);
         this.id = id;
     }
 
-    asignarId(array) {
+    idPresta(array) {
         this.id = array.length;
     }
 }
-// Fin Clase
+// Fin Clases
 
 // Inicio Constructores
 const plazosFijos = [
@@ -29,39 +30,41 @@ const plazosFijos = [
 const prestamos = [
 
 ]
-// Fin Constructor
+// Fin Constructores
 
 //Función para ejecutar el plazo fijo.
 function plazoFijo() {
     // Pedir datos y guardarlos al Array
-    let ingreso = prompt('Ingresar los datos solicitados separados por un(-):\n1)_Monto (Solo números y mayor a $1000) - Días (Mínimo 30 días - Máximo 365 días). \n2)_Ingresa x para volver al menú principal.');
+    let ingreso = prompt('Ingresar los datos solicitados separados por un(-):\nMonto (Solo números y mayor a $1000) - Días (Mínimo 30 días - Máximo 365 días). \nIngresa x para volver al menú principal.');
 
     if (ingreso.toUpperCase() != 'X') {
 
         let datos = ingreso.split('-');
         const plazo = new PlazoFijo(datos[0], datos[1]);
         plazosFijos.push(plazo);
-        plazo.asignarId(plazosFijos);
+        plazo.idPlazo(plazosFijos);
 
         let tna = 75 / 100;
 
-        if (plazo.deposito >= 1000) {
-            if (plazo.duracion >= '30') {
+        if (plazo.deposito >= 1000 && plazo.duracion >= '30') {
 
-                let costoFinal = plazo.deposito * (1 + tna * (plazo.duracion / 365));
-                let interes = costoFinal - plazo.deposito;
+            let costoFinal = plazo.deposito * (1 + tna * (plazo.duracion / 365));
+            let interes = costoFinal - plazo.deposito;
 
-                console.log(plazosFijos);
+            console.log(plazosFijos);
 
-                alert('El plazo elegido es: ' + plazo.duracion + ' Días\nSu capital:$ ' + plazo.deposito +
-                    '\nIntereses:$ ' + interes.toFixed(2) +
-                    '\nMonto Total:$ ' + costoFinal.toFixed(2) +
-                    '\nTNA: ' + tna.toFixed(2) + '%');
-            } else {
-                alert('Ingrese un valor de días mayor a 30');
+            alert('El plazo elegido es: ' + plazo.duracion + ' Días\nSu capital:$ ' + plazo.deposito +
+                '\nIntereses:$ ' + interes.toFixed(2) +
+                '\nMonto Total:$ ' + costoFinal.toFixed(2) +
+                '\nTNA: ' + tna.toFixed(2) + '%');
+
+            let reIngreso = prompt('Desea realizar otro Plazo?\n1)-Si\n2)-No');
+            if (reIngreso == 1) {
+                plazoFijo();
             }
         } else {
-            alert('Ingrese valor mayor a $1000.');
+            alert('Uno de los valores es erroneo, por favor vuelva a ingresarlos.');
+            plazoFijo();
         }
     }
 
@@ -71,57 +74,62 @@ function plazoFijo() {
 //Función para ejecutar el prestamo.
 function prestamo() {
     let porcentajeInteres = parseFloat();
-    let meses = parseInt();
-    let monto = prompt('Ingrese el monto(Maximo $200.000):$ ');
+    // Pedir datos y guardarlos al Array
+    let ingreso = prompt('Ingresar los datos solicitados separados por un(-):\nMonto a solicitar(Maximo $200.000) y Cantidad de Meses(12-24-36-48-60):$ \nIngresa x para volver al menú principal');
 
-    const presta = new Prestamo(monto[0]);
-    prestamos.push(presta);
-    presta.asignarId(prestamos);
+    if (ingreso.toUpperCase() != 'X') {
 
-    if (presta.monto <= 200000) {
+        let datos = ingreso.split('-');
+        const presta = new Prestamo(datos[0], datos[1]);
+        prestamos.push(presta);
+        presta.idPresta(prestamos);
 
-        let plazo = prompt('Por favor elije una plazo: \n1- 12 Meses. \n2- 24 Meses. \n3- 36 Meses \n4- 48 Meses \n5- 60 Meses.');
+        if (presta.monto <= 200000) {
 
-        switch (plazo) {
-            case '1':
-                porcentajeInteres = 0.25;
-                meses = 12;
-                break;
+            switch (presta.meses) {
+                case 12:
+                    porcentajeInteres = 0.25;
+                    break;
 
-            case '2':
-                porcentajeInteres = 0.40;
-                meses = 24;
-                break;
+                case 24:
+                    porcentajeInteres = 0.40;
+                    break;
 
-            case '3':
-                porcentajeInteres = 0.55;
-                meses = 36;
-                break;
+                case 36:
+                    porcentajeInteres = 0.55;
+                    break;
 
-            case '4':
-                porcentajeInteres = 0.70;
-                meses = 48;
-                break;
+                case 48:
+                    porcentajeInteres = 0.70;
+                    break;
 
-            case '5':
-                porcentajeInteres = 0.80;
-                meses = 60;
-                break;
+                case 60:
+                    porcentajeInteres = 0.80;
+                    break;
 
-            default:
-                alert('Elegiste una opción inválida');
-                break;
+                default:
+                    alert('Cántidad de meses inválida, revisa y vuelve a ingresar');
+                    break;
+            }
+
+            console.log(prestamos);
+
+            let interes = presta.monto * porcentajeInteres;
+            let total = presta.monto + interes;
+            let pagoMensual = total / presta.meses;
+
+            alert('El monto solicitado es:$ ' + presta.monto +
+                '\nSu plazo: ' + presta.meses + ' Meses\nEl Total:$ ' + total.toFixed(2) +
+                '\nPago Final:$ ' + pagoMensual.toFixed(2));
+
+            let reIngreso = prompt('Desea realizar otro Préstamo?\n1)-Si\n2)-No');
+            if (reIngreso == 1) {
+                prestamo();
+            }
+        } else {
+            alert('Supero el monto máximo permitido, vuelva a ingresar!');
+            prestamo();
         }
-
-        let interes = presta.monto * porcentajeInteres;
-        let total = presta.monto + interes;
-        let pagoMensual = total / meses;
-
-        alert('El monto solicitado es:$ ' + presta.monto +
-            '\nSu plazo: ' + meses + ' Meses\nEl Total:$ ' + total.toFixed(2) +
-            '\nPago Final:$ ' + pagoMensual.toFixed(2));
-    } else {
-        alert('Supero el monto máximo, vuelva a ingresar!');
     }
 }
 // Finaliza Función Prestamo.
